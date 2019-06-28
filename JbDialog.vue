1878813c87a7d07d23974fd1c23863a178902063
Joabe Machado <joabemachadodeabreu@gmail.com>
2019-06-28 14:36:34 -0300

    commit

--- JbDialog.vue
+++ JbDialog.vue
@@ -25,9 +25,10 @@
             value:Boolean,
             persistent:Boolean, fullscreen:Boolean, maxWidth:String, titulo:String,
         },
-        data() { return {
-
-        }},
+        data() {
+            return {
+            }
+        },
         computed:{
             getTitulo(){ return this.titulo },
             mostrar(){ return this.value }
